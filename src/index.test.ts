02a788a0c40f287bd11d1c6f627993afa7e02bd2
Christopher Williams <chris.a.williams@gmail.com>
2018-12-13 16:27:53 -0500

    feat: add summary of test results via an option defaulting to true

--- src/index.test.ts
+++ src/index.test.ts
@@ -22,13 +22,56 @@ describe("junit()", () => {
 
     expect(global.fail).not.toHaveBeenCalled()
     expect(global.markdown).not.toHaveBeenCalled()
+    expect(global.warn).toHaveBeenCalledTimes(1)
+    expect(global.warn).toHaveBeenCalledWith(
+      ":mag: Can't find junit reports at `./build/reports/**/TESTS*.xml`, skipping generating JUnit Report."
+    )
+  })
+
+  it("Gives success summary when no failures", async () => {
+    await junit({
+      pathToReport: "./fixtures/junit_success.xml",
+    })
+
+    expect(global.message).toHaveBeenCalledTimes(1)
+    expect(global.message).toHaveBeenCalledWith(
+      ":white_check_mark: All tests are passing\nNice one! All 3 tests are passing."
+    )
+    expect(global.fail).toHaveBeenCalledTimes(0)
+    expect(global.markdown).toHaveBeenCalledTimes(0)
   })
 
   it("Checks test failures were failed", async () => {
     await junit({
-      pathToReport: "./fixtures/junit*.xml",
+      pathToReport: "./fixtures/junit_failures.xml",
     })
 
+    expect(global.message).toHaveBeenCalledTimes(1)
+    expect(global.message).toHaveBeenCalledWith(
+      ":x: 1 tests have failed\nThere are 1 tests failing and 3 skipped out of 19 total tests."
+    )
+    expect(global.fail).toHaveBeenCalledTimes(1)
+    expect(global.fail).toHaveBeenCalledWith("Tests have failed, see below for more information.")
+    expect(global.markdown).toHaveBeenCalledTimes(1)
+    expect(global.markdown).toHaveBeenCalledWith(
+      `### Tests:
+
+| Classname | Name | Time | Error |
+| --- | --- | --- | --- |
+| android.Titanium.UI.Window | .safeAreaPadding with extendSafeArea true | 0.052 | expected 0 to be above 0 |
+`
+    )
+  })
+
+  it("Combines multiple report files", async () => {
+    await junit({
+      pathToReport: "./fixtures/junit_*.xml",
+    })
+
+    expect(global.message).toHaveBeenCalledTimes(1)
+    expect(global.message).toHaveBeenCalledWith(
+      ":x: 1 tests have failed\nThere are 1 tests failing and 3 skipped out of 22 total tests."
+    )
     expect(global.fail).toHaveBeenCalledTimes(1)
     expect(global.fail).toHaveBeenCalledWith("Tests have failed, see below for more information.")
     expect(global.markdown).toHaveBeenCalledTimes(1)
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as fs from "fs-extra"
 import * as glob from "glob"
-import { promisify } from "util"
+import { inspect, promisify } from "util"
 import { DOMParser } from "xmldom"
 
 // Provides dev-time type structures for  `danger` - doesn't affect runtime.
@@ -11,34 +11,71 @@ export declare function warn(message: string): void
 export declare function fail(message: string): void
 export declare function markdown(message: string): void
 
+interface Attribute {
+  nodeName: string
+}
+
+interface Element {
+  attributes: Attribute[]
+  hasAttribute(name: string): boolean
+  getAttribute(name: string): string
+  getElementsByTagName(name: string): ElementList
+  hasChildNodes(): boolean
+}
+
+interface ElementList {
+  readonly length: number
+  item(index: number): Element
+  [index: number]: Element
+}
+
 interface JUnitReportOptions {
   /**
    * The path to the generated junit files.
    */
   pathToReport?: string
+  /**
+   * Whether the test summary message will be reported using danger's `message()`. Defaults to true.
+   */
+  showMessageTestSummary?: boolean
 }
+
 /**
  * Add your Junit XML test failures to Danger
  */
 export default async function junit(options: JUnitReportOptions) {
   const currentPath: string =
     options.pathToReport !== undefined ? options.pathToReport! : "./build/reports/**/TESTS*.xml"
+  const shouldShowMessageTestSummary: boolean =
+    options.showMessageTestSummary !== undefined ? options.showMessageTestSummary! : true
+
   // Use glob to find xml reports!
   const matches: string[] = await promisify(glob)(currentPath)
   if (matches.length === 0) {
-    return
-  }
-  // Give details on failed mocha suite tests
-  const failed = await Promise.all(matches.map(m => gatherFailedTestcases(m)))
-  // flatten all the results into one array
-  const failuresAndErrors: any[] = failed.reduce((acc, val) => acc.concat(val), [])
-  if (failuresAndErrors.length === 0) {
+    warn(`:mag: Can't find junit reports at \`${currentPath}\`, skipping generating JUnit Report.`)
     return
   }
 
+  // Gather all the suites up
+  const allSuites = await Promise.all(matches.map(m => gatherSuites(m)))
+  const suites: Element[] = allSuites.reduce((acc, val) => acc.concat(val), [])
+
+  // Give a summary message
+  if (shouldShowMessageTestSummary) {
+    reportSummary(suites)
+  }
+
+  // Give details on failed tests
+  const failuresAndErrors: Element[] = gatherFailedTestcases(suites)
+  if (failuresAndErrors.length !== 0) {
+    reportFailures(failuresAndErrors)
+  }
+}
+
+function reportFailures(failuresAndErrors: Element[]): void {
   fail("Tests have failed, see below for more information.")
   let testResultsTable: string = "### Tests:\n\n"
-  const keys: string[] = Array.from(failuresAndErrors[0].attributes).map((attr: any) => attr.nodeName)
+  const keys: string[] = Array.from(failuresAndErrors[0].attributes).map((attr: Attribute) => attr.nodeName)
   const attributes: string[] = keys.map(key => {
     return key.substr(0, 1).toUpperCase() + key.substr(1).toLowerCase()
   })
@@ -70,8 +107,32 @@ export default async function junit(options: JUnitReportOptions) {
   markdown(testResultsTable)
 }
 
-// Report test failures
-async function gatherFailedTestcases(reportPath) {
+function reportSummary(suites: Element[]): void {
+  const results = {
+    count: 0,
+    failures: 0,
+    skipped: 0,
+  }
+  // for each test suite, look at:
+  // tests="19" failures="1" skipped="3" timestamp="" time="6.487">
+  suites.forEach(s => {
+    results.count += s.hasAttribute("tests") ? parseInt(s.getAttribute("tests"), 10) : 0
+    results.failures += s.hasAttribute("failures") ? parseInt(s.getAttribute("failures"), 10) : 0
+    results.failures += s.hasAttribute("errors") ? parseInt(s.getAttribute("errors"), 10) : 0
+    results.skipped += s.hasAttribute("skipped") ? parseInt(s.getAttribute("skipped"), 10) : 0
+  })
+
+  if (results.failures !== 0) {
+    message(`:x: ${results.failures} tests have failed
+There are ${results.failures} tests failing and ${results.skipped} skipped out of ${results.count} total tests.`)
+  } else {
+    message(`:white_check_mark: All tests are passing
+Nice one! All ${results.count} tests are passing.`)
+  }
+  // TODO: What about when we have none failing, but some skipped?
+}
+
+async function gatherSuites(reportPath: string): Promise<Element[]> {
   const exists = await fs.pathExists(reportPath)
   if (!exists) {
     return []
@@ -80,8 +141,11 @@ async function gatherFailedTestcases(reportPath) {
   const doc = new DOMParser().parseFromString(contents, "text/xml")
   const suiteRoot =
     doc.documentElement.firstChild.tagName === "testsuites" ? doc.documentElement.firstChild : doc.documentElement
-  const suites: any[] = Array.from(suiteRoot.getElementsByTagName("testsuite"))
+  return Array.from(suiteRoot.getElementsByTagName("testsuite"))
+}
 
+// Report test failures
+function gatherFailedTestcases(suites: Element[]): Element[] {
   // We need to get the 'testcase' elements that have an 'error' or 'failure' child node
   const failedSuites = suites.filter(suite => {
     const hasFailures = suite.hasAttribute("failures") && parseInt(suite.getAttribute("failures"), 10) !== 0
@@ -89,7 +153,7 @@ async function gatherFailedTestcases(reportPath) {
     return hasFailures || hasErrors
   })
   // Gather all the testcase nodes from each failed suite properly.
-  let failedSuitesAllTests: any[] = []
+  let failedSuitesAllTests: Element[] = []
   failedSuites.forEach(suite => {
     failedSuitesAllTests = failedSuitesAllTests.concat(Array.from(suite.getElementsByTagName("testcase")))
   })
